33f61e39f59fc3b882e3103604b3f4d771fd85cf
Mark Spanbroek <mark@spanbroek.net>
2023-01-30 14:35:14 +0100

    [ci] Run fuzzing in CI

--- fuzzing/fuzz.sh
+++ fuzzing/fuzz.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 set -e
-echidna-test . --contract FuzzMarketplace --config fuzzing/echidna.yaml
+
+if command -v echidna-test; then
+  fuzz () {
+    echidna-test . \
+      --config fuzzing/echidna.yaml \
+      --contract $1
+  }
+else
+  fuzz () {
+    docker run \
+      --rm \
+      -v `pwd`:/src ghcr.io/crytic/echidna/echidna \
+      bash -c \
+        "cd /src && echidna-test . \
+          --config fuzzing/echidna.yaml \
+          --crytic-args --ignore-compile \
+          --contract $1"
+  }
+fi
+
+fuzz FuzzMarketplace
