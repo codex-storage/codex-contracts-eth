3390e21071a85e15515e579662f7b73f4837484e
Mark Spanbroek <mark@spanbroek.net>
2023-01-30 10:32:30 +0100

    [fuzzing] Enable fuzzing for Marketplace
    Replaces runtime invariant checks with fuzzing tests,
    simplifying the contract code and lowering gas costs.

---
+++ contracts/FuzzMarketplace.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./TestToken.sol";
+import "./Marketplace.sol";
+
+contract FuzzMarketplace is Marketplace {
+  constructor()
+    Marketplace(
+      new TestToken(),
+      MarketplaceConfig(CollateralConfig(10, 5, 3, 10), ProofConfig(10, 5, 64))
+    )
+  // solhint-disable-next-line no-empty-blocks
+  {
+
+  }
+
+  // Properties to be tested through fuzzing
+
+  MarketplaceTotals private _lastSeenTotals;
+
+  function neverDecreaseTotals() public {
+    assert(_marketplaceTotals.received >= _lastSeenTotals.received);
+    assert(_marketplaceTotals.sent >= _lastSeenTotals.sent);
+    _lastSeenTotals = _marketplaceTotals;
+  }
+
+  function neverLoseFunds() public view {
+    uint256 total = _marketplaceTotals.received - _marketplaceTotals.sent;
+    assert(token.balanceOf(address(this)) >= total);
+  }
+}
