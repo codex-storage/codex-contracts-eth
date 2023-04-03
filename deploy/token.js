63d8ec786acd84087891484b0d4febf58437d7e5
Adam Uhlíř <adam@uhlir.dev>
2023-04-03 10:39:48 +0200

    build: funds localhost env. accounts (#52)

--- deploy/token.js
+++ deploy/token.js
@@ -1,6 +1,17 @@
-module.exports = async ({ deployments, getNamedAccounts }) => {
+const MINTED_TOKENS = 1_000_000_000
+
+module.exports = async ({ deployments, getNamedAccounts, getUnnamedAccounts }) => {
   const { deployer } = await getNamedAccounts()
-  await deployments.deploy("TestToken", { from: deployer })
+  const tokenDeployment = await deployments.deploy("TestToken", { from: deployer })
+  const token = await hre.ethers.getContractAt("TestToken", tokenDeployment.address)
+
+  const accounts = [...Object.values(await getNamedAccounts()), ...(await getUnnamedAccounts())]
+  for (const account of accounts) {
+    console.log(`Minting ${MINTED_TOKENS} tokens to address ${account}`)
+
+    await token.mint(account, MINTED_TOKENS, { from: deployer })
+  }
+
 }
 
 module.exports.tags = ["TestToken"]
