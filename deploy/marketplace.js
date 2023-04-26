30affa0da85985f6dc90b62f6293de46a9e26130
Adam Uhlíř <adam@uhlir.dev>
2023-04-26 09:44:55 +0200

    build: make sure marketplace is on predefined address (#54)

--- deploy/marketplace.js
+++ deploy/marketplace.js
@@ -1,3 +1,5 @@
+const MARKETPLACE_HARDCODED_ADDRESS = "0x59b670e9fA9D0A427751Af201D676719a970857b"
+
 async function deployMarketplace({ deployments, getNamedAccounts }) {
   const token = await deployments.get("TestToken")
   const configuration = {
@@ -24,9 +26,23 @@ async function mine256blocks({ network, ethers }) {
   }
 }
 
+async function aliasContract({deployments, network}) {
+  if (network.tags.local) {
+    const marketplaceDeployment = await deployments.get("Marketplace")
+
+    if (marketplaceDeployment.address === MARKETPLACE_HARDCODED_ADDRESS) {
+      return 
+    }
+
+    console.log(`Aliasing marketplace from address ${marketplaceDeployment.address} to ${MARKETPLACE_HARDCODED_ADDRESS}`)
+    await ethers.provider.send("hardhat_setCode", [MARKETPLACE_HARDCODED_ADDRESS, marketplaceDeployment.address])
+  }
+}
+
 module.exports = async (environment) => {
   await mine256blocks(environment)
   await deployMarketplace(environment)
+  await aliasContract(environment)
 }
 
 module.exports.tags = ["Marketplace"]
