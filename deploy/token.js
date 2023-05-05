49ba9834b4e02e004de000e1b9d0546beade4bb2
Eric Mastro <github@egonat.me>
2023-05-05 16:00:26 +1000

    only mint tokens when on local hardhat network

--- deploy/token.js
+++ deploy/token.js
@@ -1,16 +1,18 @@
 const MINTED_TOKENS = 1_000_000_000
 
-module.exports = async ({ deployments, getNamedAccounts, getUnnamedAccounts }) => {
+module.exports = async ({ deployments, getNamedAccounts, getUnnamedAccounts, network }) => {
   const { deployer } = await getNamedAccounts()
   const tokenDeployment = await deployments.deploy("TestToken", { from: deployer })
   const token = await hre.ethers.getContractAt("TestToken", tokenDeployment.address)
 
   const accounts = [...Object.values(await getNamedAccounts()), ...(await getUnnamedAccounts())]
-  for (const account of accounts) {
-    console.log(`Minting ${MINTED_TOKENS} tokens to address ${account}`)
+  if (network.tags.local) {
+    for (const account of accounts) {
+      console.log(`Minting ${MINTED_TOKENS} tokens to address ${account}`)
 
-    const transaction = await token.mint(account, MINTED_TOKENS, { from: deployer })
-    await transaction.wait()
+      const transaction = await token.mint(account, MINTED_TOKENS, { from: deployer })
+      await transaction.wait()
+    }
   }
 }
 
