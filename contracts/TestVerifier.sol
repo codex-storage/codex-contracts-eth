e1657acdd0c2f4e831e3252efb2ce8a5215c7f62
Mark Spanbroek <mark@spanbroek.net>
2024-01-15 16:28:27 +0100

    Stub out zk proof verification in marketplace tests

--- contracts/TestVerifier.sol
+++ contracts/TestVerifier.sol
@@ -4,12 +4,22 @@ pragma solidity ^0.8.8;
 import "./Verifier.sol";
 
 contract TestVerifier is IVerifier {
+  bool private _proofsAreValid;
+
+  constructor() {
+    _proofsAreValid = true;
+  }
+
+  function setProofsAreValid(bool proofsAreValid) public {
+    _proofsAreValid = proofsAreValid;
+  }
+
   function verifyProof(
     uint[2] calldata,
     uint[2][2] calldata,
     uint[2] calldata,
     uint[3] calldata
-  ) external pure returns (bool) {
-    return false;
+  ) external view returns (bool) {
+    return _proofsAreValid;
   }
 }
