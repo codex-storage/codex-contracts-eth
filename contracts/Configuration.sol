f8cfdf665fd09b3fc27709c94aaecd76d32c11cf
Eric <5089238+emizzle@users.noreply.github.com>
2024-08-05 16:45:18 +1000

    add `downtimeProduct` configuration parameter

--- contracts/Configuration.sol
+++ contracts/Configuration.sol
@@ -24,4 +24,8 @@ struct ProofConfig {
   uint256 timeout; // mark proofs as missing before the timeout (in seconds)
   uint8 downtime; // ignore this much recent blocks for proof requirements
   string zkeyHash; // hash of the zkey file which is linked to the verifier
+  // Ensures the pointer does not remain in downtime for many consecutive
+  // periods. For each period increase, move the pointer `pointerProduct`
+  // blocks. Should be a prime number to ensure there are no cycles.
+  uint8 downtimeProduct;
 }
--- contracts/FuzzMarketplace.sol
+++ contracts/FuzzMarketplace.sol
@@ -10,7 +10,7 @@ contract FuzzMarketplace is Marketplace {
     Marketplace(
       MarketplaceConfig(
         CollateralConfig(10, 5, 3, 10),
-        ProofConfig(10, 5, 64, "")
+        ProofConfig(10, 5, 64, "", 67)
       ),
       new TestToken(),
       new TestVerifier()
