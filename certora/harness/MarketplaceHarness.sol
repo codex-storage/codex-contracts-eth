2e3f775a0da8a3b7d5d018233176563f888a41cf
r4bbit <445106+0x-r4bbit@users.noreply.github.com>
2024-08-05 16:27:20 +0200

    chore: formally verify request state changes
    This commit adds CVL rule that formally verifies the state changes of
    any given request in relation to the functions of the contract that can
    cause them.
    
    Closes #128

---
+++ certora/harness/MarketplaceHarness.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.23;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IGroth16Verifier} from "../../contracts/Groth16.sol";
+import {MarketplaceConfig} from "../../contracts/Configuration.sol";
+import {Marketplace} from "../../contracts/Marketplace.sol";
+import {RequestId} from "../../contracts/Requests.sol";
+
+contract MarketplaceHarness is Marketplace {
+    constructor(MarketplaceConfig memory config, IERC20 token, IGroth16Verifier verifier) Marketplace(config, token, verifier) {}
+
+    function requestContext(RequestId requestId) public returns (Marketplace.RequestContext memory) {
+        return _requestContexts[requestId];
+    }
+}
+
