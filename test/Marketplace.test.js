f0619864235b758b557fc51e1ddefd7f9b7f1723
Adam Uhlíř <adam@uhlir.dev>
2024-08-20 11:34:51 +0200

    test: fixes states changes

--- test/Marketplace.test.js
+++ test/Marketplace.test.js
@@ -744,7 +744,7 @@ describe("Marketplace", function () {
       switchAccount(client)
       await expect(
         marketplace.withdrawFunds(slot.request, clientWithdrawRecipient.address)
-      ).to.be.revertedWith("Request not yet timed out")
+      ).to.be.revertedWith("Invalid state")
     })
 
     it("rejects withdraw when wrong account used", async function () {
@@ -902,8 +902,8 @@ describe("Marketplace", function () {
 
       const expectedHostPayout = payoutForDuration(
         request,
-        (await marketplace.requestEnd(requestId(request))).toNumber(),
-        filledAt
+        filledAt,
+        (await marketplace.requestEnd(requestId(request))).toNumber()
       )
 
       switchAccount(client)
