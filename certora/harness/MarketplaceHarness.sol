ebdf9ed366a26cfe5b67ac3bed04f9837c7dca1a
r4bbit <445106+0x-r4bbit@users.noreply.github.com>
2024-08-21 20:22:39 +0200

    fix(certora): fix rule that missed slots == number of issed periods

--- certora/harness/MarketplaceHarness.sol
+++ certora/harness/MarketplaceHarness.sol
@@ -6,13 +6,17 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {IGroth16Verifier} from "../../contracts/Groth16.sol";
 import {MarketplaceConfig} from "../../contracts/Configuration.sol";
 import {Marketplace} from "../../contracts/Marketplace.sol";
-import {RequestId} from "../../contracts/Requests.sol";
+import {RequestId, SlotId} from "../../contracts/Requests.sol";
 
 contract MarketplaceHarness is Marketplace {
     constructor(MarketplaceConfig memory config, IERC20 token, IGroth16Verifier verifier) Marketplace(config, token, verifier) {}
 
     function requestContext(RequestId requestId) public returns (Marketplace.RequestContext memory) {
         return _requestContexts[requestId];
+    }
+
+    function slots(SlotId slotId) public returns (Marketplace.Slot memory) {
+        return _slots[slotId];
     }
 
     function publicPeriodEnd(Period period) public view returns (uint256) {
