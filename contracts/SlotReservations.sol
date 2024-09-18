b62c72b5e1a348c2d47c9f3c31c1d712e37286b7
Eric <5089238+emizzle@users.noreply.github.com>
2024-09-18 20:22:17 +1000

    uses msg.sender for slot reservation address

--- contracts/SlotReservations.sol
+++ contracts/SlotReservations.sol
@@ -7,20 +7,19 @@ import "./Requests.sol";
 contract SlotReservations {
   using EnumerableSet for EnumerableSet.AddressSet;
 
-  mapping(SlotId => EnumerableSet.AddressSet) private _reservations;
+  mapping(SlotId => EnumerableSet.AddressSet) internal _reservations;
 
   uint8 private constant _MAX_RESERVATIONS = 3;
 
-  function reserveSlot(SlotId slotId, address host) public returns (bool) {
-    require(canReserveSlot(slotId, host), "Reservation not allowed");
+  function reserveSlot(SlotId slotId) public returns (bool) {
+    address host = msg.sender;
+    require(canReserveSlot(slotId), "Reservation not allowed");
     // returns false if set already contains address
     return _reservations[slotId].add(host);
   }
 
-  function canReserveSlot(
-    SlotId slotId,
-    address host
-  ) public view returns (bool) {
+  function canReserveSlot(SlotId slotId) public view returns (bool) {
+    address host = msg.sender;
     return
       // TODO: add in check for address inside of expanding window
       (_reservations[slotId].length() < _MAX_RESERVATIONS) &&
