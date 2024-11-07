11ccefd720f1932608b67db95af5b72d73d1257b
Mark Spanbroek <mark@spanbroek.net>
2024-11-07 15:27:22 +0100

    Different configurations for different networks
    By default we have a proof period of 2 minutes,
    but on hardhat it's 1 minute.

---
+++ configuration/configuration.js
@@ -0,0 +1,33 @@
+const fs = require("fs")
+
+const BASE_PATH = __dirname + "/networks"
+
+const DEFAULT_CONFIGURATION = {
+  collateral: {
+    repairRewardPercentage: 10,
+    maxNumberOfSlashes: 2,
+    slashCriterion: 2,
+    slashPercentage: 20,
+  },
+  proofs: {
+    // period has to be less than downtime * blocktime
+    period: 120, // seconds
+    timeout: 30, // seconds
+    downtime: 64, // number of blocks
+    downtimeProduct: 67 // number of blocks
+  },
+  reservations: {
+    maxReservations: 3
+  }
+}
+
+function loadConfiguration(name) {
+  const path = `${BASE_PATH}/${name}/configuration.js`
+  if(fs.existsSync(path)) {
+    return require(path)
+  } else {
+    return DEFAULT_CONFIGURATION
+  }
+}
+
+module.exports = { loadConfiguration }
