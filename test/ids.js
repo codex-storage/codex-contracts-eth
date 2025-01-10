1626f0ce96b29dd12884ae1a67d77d47812106c2
Adam Uhlíř <adam@uhlir.dev>
2025-01-10 08:30:41 +0100

    chore: feedback
    Co-authored-by: markspanbroek <mark@spanbroek.net>

--- test/ids.js
+++ test/ids.js
@@ -2,9 +2,9 @@ const { ethers } = require("hardhat")
 const { keccak256, defaultAbiCoder } = ethers.utils
 
 function requestId(request) {
-  const Ask = "tuple(uint256, uint256, uint256, int64, int64, int64, int64)"
+  const Ask = "tuple(uint256, uint256, uint256, uint64, uint64, uint64, int64)"
   const Content = "tuple(bytes, bytes32)"
-  const Request = "tuple(address, " + Ask + ", " + Content + ", int64, bytes32)"
+  const Request = "tuple(address, " + Ask + ", " + Content + ", uint64, bytes32)"
   return keccak256(defaultAbiCoder.encode([Request], requestToArray(request)))
 }
 
