5fa14ec0c64d03b73909d4a812fe205f4c7dd389
Adam Uhlíř <adam@uhlir.dev>
2025-01-15 11:08:51 +0100

    test: fix

--- test/SlotReservations.test.js
+++ test/SlotReservations.test.js
@@ -85,7 +85,7 @@ describe("SlotReservations", function () {
     await reservations.reserveSlot(reqId, slotIndex)
     switchAccount(provider)
     await expect(reservations.reserveSlot(reqId, slotIndex)).to.be.revertedWith(
-      "Reservation not allowed"
+      "SlotReservations_ReservationNotAllowed"
     )
     expect(await reservations.length(id)).to.equal(3)
     expect(await reservations.contains(id, provider.address)).to.be.false
@@ -105,7 +105,7 @@ describe("SlotReservations", function () {
   it("cannot reserve a slot if not free", async function () {
     await reservations.setSlotState(id, SlotState.Filled)
     await expect(reservations.reserveSlot(reqId, slotIndex)).to.be.revertedWith(
-      "Reservation not allowed"
+      "SlotReservations_ReservationNotAllowed"
     )
     expect(await reservations.length(id)).to.equal(0)
   })
