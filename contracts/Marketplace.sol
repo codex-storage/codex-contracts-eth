60e217fc687a7306a64b52a1bcf8633746543609
Arnaud <arnaud@status.im>
2025-01-17 14:29:10 +0100

    Replace assert with revert

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -376,8 +376,13 @@ contract Marketplace is SlotReservations, Proofs, StateRetrieval, Endian {
     uint256 collateralAmount = slot.currentCollateral;
     _marketplaceTotals.sent += (payoutAmount + collateralAmount);
     slot.state = SlotState.Paid;
-    assert(_token.transfer(rewardRecipient, payoutAmount));
-    assert(_token.transfer(collateralRecipient, collateralAmount));
+    if (!_token.transfer(rewardRecipient, payoutAmount)) {
+      revert Marketplace_TransferFailed();
+    }
+
+    if (!_token.transfer(collateralRecipient, collateralAmount)) {
+      revert Marketplace_TransferFailed();
+    }
   }
 
   /**
@@ -406,8 +411,12 @@ contract Marketplace is SlotReservations, Proofs, StateRetrieval, Endian {
     uint256 collateralAmount = slot.currentCollateral;
     _marketplaceTotals.sent += (payoutAmount + collateralAmount);
     slot.state = SlotState.Paid;
-    assert(_token.transfer(rewardRecipient, payoutAmount));
-    assert(_token.transfer(collateralRecipient, collateralAmount));
+    if (!_token.transfer(rewardRecipient, payoutAmount)) {
+      revert Marketplace_TransferFailed();
+    }
+    if (!_token.transfer(collateralRecipient, collateralAmount)) {
+      revert Marketplace_TransferFailed();
+    }
   }
 
   /**
@@ -473,7 +482,10 @@ contract Marketplace is SlotReservations, Proofs, StateRetrieval, Endian {
 
     uint256 amount = context.fundsToReturnToClient;
     _marketplaceTotals.sent += amount;
-    assert(_token.transfer(withdrawRecipient, amount));
+
+    if (!_token.transfer(withdrawRecipient, amount)) {
+      revert Marketplace_TransferFailed();
+    }
 
     // We zero out the funds tracking in order to prevent double-spends
     context.fundsToReturnToClient = 0;
