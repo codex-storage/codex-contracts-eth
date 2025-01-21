3c84fd6701bc45d84f0da47e53a27618cafcdc82
Adam Uhlíř <adam@uhlir.dev>
2025-01-21 17:03:27 +0100

    test(certora): payout harness

--- certora/harness/MarketplaceHarness.sol
+++ certora/harness/MarketplaceHarness.sol
@@ -6,23 +6,145 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {IGroth16Verifier} from "../../contracts/Groth16.sol";
 import {MarketplaceConfig} from "../../contracts/Configuration.sol";
 import {Marketplace} from "../../contracts/Marketplace.sol";
-import {RequestId, SlotId} from "../../contracts/Requests.sol";
-import {Requests} from "../../contracts/Requests.sol";
+import {Requests, Request, RequestState, SlotState, RequestId, SlotId} from "../../contracts/Requests.sol";
 
 contract MarketplaceHarness is Marketplace {
-    constructor(MarketplaceConfig memory config, IERC20 token, IGroth16Verifier verifier)
-        Marketplace(config, token, verifier)
-    {}
+  using Requests for Request;
+
+  constructor(
+    MarketplaceConfig memory config,
+    IERC20 token,
+    IGroth16Verifier verifier
+  ) Marketplace(config, token, verifier) {}
 
     function publicPeriodEnd(Period period) public view returns (uint64) {
         return _periodEnd(period);
     }
 
-    function slots(SlotId slotId) public view returns (Slot memory) {
-        return _slots[slotId];
+  function slots(SlotId slotId) public view returns (Slot memory) {
+    return _slots[slotId];
+  }
+
+  function generateSlotId(
+    RequestId requestId,
+    uint64 slotIndex
+  ) public pure returns (SlotId) {
+    return Requests.slotId(requestId, slotIndex);
+  }
+
+  /******************/
+  /* FUNDS TRACKING */
+  /******************/
+
+  // TODO: Once reward for submitting missing proof call is implemented, add it here
+  // https://github.com/codex-storage/codex-contracts-eth/issues/70
+
+  /**
+   * View that returns allocated funds to the host for given request and its slot
+   * based on the state of the request/slot.
+   */
+  function hostRequestSlotFunds(
+    RequestId requestId,
+    uint256 slotIndex
+  ) public view returns (uint256) {
+    SlotId slotId = Requests.slotId(requestId, slotIndex);
+    Slot memory slot = _slots[slotId];
+    SlotState state = slotState(slotId);
+
+    // Payout to hosts is allocated only upon successfully finishing of the Request
+    // or partial payout when Request expires
+    if (
+      state == SlotState.Free ||
+      state == SlotState.Filled ||
+      state == SlotState.Failed ||
+      state == SlotState.Paid
+    ) {
+      return 0;
+    } else if (state == SlotState.Finished) {
+      return _slotPayout(requestId, slot.filledAt);
+    } else if (state == SlotState.Cancelled) {
+      return _slotPayout(requestId, slot.filledAt, requestExpiry(requestId));
     }
 
-    function generateSlotId(RequestId requestId, uint64 slotIndex) public pure returns (SlotId) {
-        return Requests.slotId(requestId, slotIndex);
+    revert("Invalid state");
+  }
+
+  /**
+   * View that returns allocated funds to the client for given request based
+   * on the state of the request.
+   */
+  function clientRequestFunds(
+    RequestId requestId
+  ) public view returns (uint256) {
+    Request memory request = _requests[requestId];
+    RequestState state = requestState(requestId);
+
+    if (
+      state == RequestState.New ||
+      state == RequestState.Started ||
+      state == RequestState.Failed
+    ) {
+      return request.maxPrice();
+    } else if (
+      state == RequestState.Finished || state == RequestState.Cancelled
+    ) {
+      return _requestContexts[requestId].fundsToReturnToClient;
     }
+
+    revert("Invalid state");
+  }
+
+  /**
+   * View that returns all allocated funds for paying out Request funds
+   * Eq. funds that are coming in from Client that pays for Request to be stored
+   * and then is splitted between hosts and client based on how the hosting of
+   * the Request goes (eq. base on state).
+   */
+  function allocatedRequestFunds(
+    RequestId requestId
+  ) public view returns (uint256) {
+    uint256 slotsFunds = 0;
+    for (uint64 i = 0; i < _requests[requestId].ask.slots; i++) {
+      slotsFunds += hostRequestSlotFunds(requestId, i);
+    }
+
+    return slotsFunds + clientRequestFunds(requestId);
+  }
+
+  function collateralRequestSlotFunds(
+    RequestId requestId,
+    uint256 slotIndex
+  ) public view returns (uint256) {
+    SlotId slotId = Requests.slotId(requestId, slotIndex);
+    Slot memory slot = _slots[slotId];
+    SlotState state = slotState(slotId);
+
+    if (
+      state == SlotState.Free ||
+      state == SlotState.Filled ||
+      state == SlotState.Failed ||
+      state == SlotState.Paid
+    ) {
+      return 0;
+    } else if (state == SlotState.Finished || state == SlotState.Cancelled) {
+      return slot.currentCollateral;
+    }
+
+    revert("Invalid state");
+  }
+
+  /**
+   * View that returns sum of all collateral funds for given request based on
+   * the state of the request and slashing that occured.
+   */
+  function collateralRequestFunds(
+    RequestId requestId
+  ) public view returns (uint256) {
+    uint256 collateralFunds = 0;
+    for (uint64 i = 0; i < _requests[requestId].ask.slots; i++) {
+      collateralFunds += collateralRequestSlotFunds(requestId, i);
+    }
+
+    return collateralFunds;
+  }
 }
--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -48,7 +48,7 @@ contract Marketplace is SlotReservations, Proofs, StateRetrieval, Endian {
   IERC20 private immutable _token;
   MarketplaceConfig private _config;
 
-  mapping(RequestId => Request) private _requests;
+  mapping(RequestId => Request) internal _requests;
   mapping(RequestId => RequestContext) internal _requestContexts;
   mapping(SlotId => Slot) internal _slots;
 
@@ -581,7 +581,7 @@ contract Marketplace is SlotReservations, Proofs, StateRetrieval, Endian {
   function _slotPayout(
     RequestId requestId,
     uint64 startingTimestamp
-  ) private view returns (uint256) {
+  ) internal view returns (uint256) {
     return
       _slotPayout(
         requestId,
@@ -595,7 +595,7 @@ contract Marketplace is SlotReservations, Proofs, StateRetrieval, Endian {
     RequestId requestId,
     uint64 startingTimestamp,
     uint64 endingTimestamp
-  ) private view returns (uint256) {
+  ) internal view returns (uint256) {
     Request storage request = _requests[requestId];
     if (startingTimestamp >= endingTimestamp)
       revert Marketplace_StartNotBeforeExpiry();
