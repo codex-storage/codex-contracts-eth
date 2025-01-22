606cfc4c1e9b6061a59a5392e8a0727b9d49e8e6
Mark Spanbroek <mark@spanbroek.net>
2025-01-22 11:32:22 +0100

    vault: allow recipient to withdraw

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -1,43 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "./Timestamps.sol";
+import "./VaultBase.sol";
 
-using SafeERC20 for IERC20;
-using Timestamps for Timestamp;
-
-contract Vault {
-  IERC20 private immutable _token;
-
-  type Controller is address;
-  type Context is bytes32;
-  type Recipient is address;
-
-  struct Lock {
-    Timestamp expiry;
-    Timestamp maximum;
-  }
-
-  mapping(Controller => mapping(Context => Lock)) private _locks;
-  mapping(Controller => mapping(Context => mapping(Recipient => uint256)))
-    private _available;
-  mapping(Controller => mapping(Context => mapping(Recipient => uint256)))
-    private _designated;
-
-  constructor(IERC20 token) {
-    _token = token;
-  }
+contract Vault is VaultBase {
+  // solhint-disable-next-line no-empty-blocks
+  constructor(IERC20 token) VaultBase(token) {}
 
   function balance(
     Context context,
     Recipient recipient
   ) public view returns (uint256) {
     Controller controller = Controller.wrap(msg.sender);
-    return
-      _available[controller][context][recipient] +
-      _designated[controller][context][recipient];
+    return _getBalance(controller, context, recipient);
   }
 
   function designated(
@@ -45,40 +20,32 @@ contract Vault {
     Recipient recipient
   ) public view returns (uint256) {
     Controller controller = Controller.wrap(msg.sender);
-    return _designated[controller][context][recipient];
+    return _getDesignated(controller, context, recipient);
   }
 
   function lock(Context context) public view returns (Lock memory) {
     Controller controller = Controller.wrap(msg.sender);
-    return _locks[controller][context];
+    return _getLock(controller, context);
   }
 
   function deposit(Context context, address from, uint256 amount) public {
     Controller controller = Controller.wrap(msg.sender);
-    Recipient recipient = Recipient.wrap(from);
-    _available[controller][context][recipient] += amount;
-    _token.safeTransferFrom(from, address(this), amount);
-  }
-
-  function _delete(Context context, Recipient recipient) private {
-    Controller controller = Controller.wrap(msg.sender);
-    delete _available[controller][context][recipient];
-    delete _designated[controller][context][recipient];
+    _deposit(controller, context, from, amount);
   }
 
   function withdraw(Context context, Recipient recipient) public {
     Controller controller = Controller.wrap(msg.sender);
-    require(!lock(context).expiry.isFuture(), Locked());
-    delete _locks[controller][context];
-    uint256 amount = balance(context, recipient);
-    _delete(context, recipient);
-    _token.safeTransfer(Recipient.unwrap(recipient), amount);
+    _withdraw(controller, context, recipient);
+  }
+
+  function withdrawByRecipient(Controller controller, Context context) public {
+    Recipient recipient = Recipient.wrap(msg.sender);
+    _withdraw(controller, context, recipient);
   }
 
   function burn(Context context, Recipient recipient) public {
-    uint256 amount = balance(context, recipient);
-    _delete(context, recipient);
-    _token.safeTransfer(address(0xdead), amount);
+    Controller controller = Controller.wrap(msg.sender);
+    _burn(controller, context, recipient);
   }
 
   function transfer(
@@ -88,12 +55,7 @@ contract Vault {
     uint256 amount
   ) public {
     Controller controller = Controller.wrap(msg.sender);
-    require(
-      amount <= _available[controller][context][from],
-      InsufficientBalance()
-    );
-    _available[controller][context][from] -= amount;
-    _available[controller][context][to] += amount;
+    _transfer(controller, context, from, to, amount);
   }
 
   function designate(
@@ -102,34 +64,16 @@ contract Vault {
     uint256 amount
   ) public {
     Controller controller = Controller.wrap(msg.sender);
-    require(
-      amount <= _available[controller][context][recipient],
-      InsufficientBalance()
-    );
-    _available[controller][context][recipient] -= amount;
-    _designated[controller][context][recipient] += amount;
+    _designate(controller, context, recipient, amount);
   }
 
   function lockup(Context context, Timestamp expiry, Timestamp maximum) public {
-    require(Timestamp.unwrap(lock(context).maximum) == 0, AlreadyLocked());
-    require(!expiry.isAfter(maximum), ExpiryPastMaximum());
     Controller controller = Controller.wrap(msg.sender);
-    _locks[controller][context] = Lock({expiry: expiry, maximum: maximum});
+    _lockup(controller, context, expiry, maximum);
   }
 
   function extend(Context context, Timestamp expiry) public {
-    Lock memory previous = lock(context);
-    require(previous.expiry.isFuture(), LockExpired());
-    require(!previous.expiry.isAfter(expiry), InvalidExpiry());
-    require(!expiry.isAfter(previous.maximum), ExpiryPastMaximum());
     Controller controller = Controller.wrap(msg.sender);
-    _locks[controller][context].expiry = expiry;
+    _extendLock(controller, context, expiry);
   }
-
-  error InsufficientBalance();
-  error Locked();
-  error AlreadyLocked();
-  error ExpiryPastMaximum();
-  error InvalidExpiry();
-  error LockExpired();
 }
