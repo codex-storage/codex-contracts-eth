effa0c9a859f1271c6beeca57c0f0b9b34c7fbea
Mark Spanbroek <mark@spanbroek.net>
2025-01-23 10:29:53 +0100

    vault: stop flowing when lock expires

--- contracts/Timestamps.sol
+++ contracts/Timestamps.sol
@@ -3,8 +3,13 @@ pragma solidity 0.8.28;
 
 type Timestamp is uint64;
 
+using {_notEquals as !=} for Timestamp global;
 using {_lessThan as <} for Timestamp global;
 using {_atMost as <=} for Timestamp global;
+
+function _notEquals(Timestamp a, Timestamp b) pure returns (bool) {
+  return Timestamp.unwrap(a) != Timestamp.unwrap(b);
+}
 
 function _lessThan(Timestamp a, Timestamp b) pure returns (bool) {
   return Timestamp.unwrap(a) < Timestamp.unwrap(b);
@@ -17,5 +22,16 @@ function _atMost(Timestamp a, Timestamp b) pure returns (bool) {
 library Timestamps {
   function currentTime() internal view returns (Timestamp) {
     return Timestamp.wrap(uint64(block.timestamp));
+  }
+
+  function earliest(
+    Timestamp a,
+    Timestamp b
+  ) internal pure returns (Timestamp) {
+    if (a <= b) {
+      return a;
+    } else {
+      return b;
+    }
   }
 }
