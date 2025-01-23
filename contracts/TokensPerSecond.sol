dc4e277a20ea8e6ff40827fa1fae039176192e86
Mark Spanbroek <mark@spanbroek.net>
2025-01-23 15:05:07 +0100

    vault: do not allow flow when lock already expired

--- contracts/TokensPerSecond.sol
+++ contracts/TokensPerSecond.sol
@@ -5,13 +5,18 @@ import "./Timestamps.sol";
 
 type TokensPerSecond is int256;
 
-using {_negate as -} for TokensPerSecond global;
-using {_equals as ==} for TokensPerSecond global;
+using {_tokensPerSecondNegate as -} for TokensPerSecond global;
+using {_tokensPerSecondEquals as ==} for TokensPerSecond global;
 
-function _negate(TokensPerSecond rate) pure returns (TokensPerSecond) {
+function _tokensPerSecondNegate(
+  TokensPerSecond rate
+) pure returns (TokensPerSecond) {
   return TokensPerSecond.wrap(-TokensPerSecond.unwrap(rate));
 }
 
-function _equals(TokensPerSecond a, TokensPerSecond b) pure returns (bool) {
+function _tokensPerSecondEquals(
+  TokensPerSecond a,
+  TokensPerSecond b
+) pure returns (bool) {
   return TokensPerSecond.unwrap(a) == TokensPerSecond.unwrap(b);
 }
