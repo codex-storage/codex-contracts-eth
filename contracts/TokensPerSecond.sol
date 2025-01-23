b3964cd1f02d906a4bb0043bbcebc5cc3589a771
Mark Spanbroek <mark@spanbroek.net>
2025-01-23 10:19:41 +0100

    vault: use custom operators to improve readability

--- contracts/TokensPerSecond.sol
+++ contracts/TokensPerSecond.sol
@@ -6,7 +6,12 @@ import "./Timestamps.sol";
 type TokensPerSecond is int256;
 
 using {_negate as -} for TokensPerSecond global;
+using {_equals as ==} for TokensPerSecond global;
 
 function _negate(TokensPerSecond rate) pure returns (TokensPerSecond) {
   return TokensPerSecond.wrap(-TokensPerSecond.unwrap(rate));
+}
+
+function _equals(TokensPerSecond a, TokensPerSecond b) pure returns (bool) {
+  return TokensPerSecond.unwrap(a) == TokensPerSecond.unwrap(b);
 }
