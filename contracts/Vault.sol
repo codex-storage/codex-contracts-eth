ff21f8bffc3fbad37bcc07f797aefc23d03a90ae
Mark Spanbroek <mark@spanbroek.net>
2025-02-03 16:45:30 +0100

    vault: reorder functions
    in roughly chronological order

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -30,24 +30,28 @@ contract Vault is VaultBase {
     return _getLock(controller, context);
   }
 
+  function lock(Context context, Timestamp expiry, Timestamp maximum) public {
+    Controller controller = Controller.wrap(msg.sender);
+    _lock(controller, context, expiry, maximum);
+  }
+
+  function extendLock(Context context, Timestamp expiry) public {
+    Controller controller = Controller.wrap(msg.sender);
+    _extendLock(controller, context, expiry);
+  }
+
   function deposit(Context context, address from, uint128 amount) public {
     Controller controller = Controller.wrap(msg.sender);
     _deposit(controller, context, from, amount);
   }
 
-  function withdraw(Context context, Recipient recipient) public {
+  function designate(
+    Context context,
+    Recipient recipient,
+    uint128 amount
+  ) public {
     Controller controller = Controller.wrap(msg.sender);
-    _withdraw(controller, context, recipient);
-  }
-
-  function withdrawByRecipient(Controller controller, Context context) public {
-    Recipient recipient = Recipient.wrap(msg.sender);
-    _withdraw(controller, context, recipient);
-  }
-
-  function burn(Context context, Recipient recipient) public {
-    Controller controller = Controller.wrap(msg.sender);
-    _burn(controller, context, recipient);
+    _designate(controller, context, recipient, amount);
   }
 
   function transfer(
@@ -60,25 +64,6 @@ contract Vault is VaultBase {
     _transfer(controller, context, from, to, amount);
   }
 
-  function designate(
-    Context context,
-    Recipient recipient,
-    uint128 amount
-  ) public {
-    Controller controller = Controller.wrap(msg.sender);
-    _designate(controller, context, recipient, amount);
-  }
-
-  function lock(Context context, Timestamp expiry, Timestamp maximum) public {
-    Controller controller = Controller.wrap(msg.sender);
-    _lockup(controller, context, expiry, maximum);
-  }
-
-  function extendLock(Context context, Timestamp expiry) public {
-    Controller controller = Controller.wrap(msg.sender);
-    _extendLock(controller, context, expiry);
-  }
-
   function flow(
     Context context,
     Recipient from,
@@ -87,5 +72,20 @@ contract Vault is VaultBase {
   ) public {
     Controller controller = Controller.wrap(msg.sender);
     _flow(controller, context, from, to, rate);
+  }
+
+  function burn(Context context, Recipient recipient) public {
+    Controller controller = Controller.wrap(msg.sender);
+    _burn(controller, context, recipient);
+  }
+
+  function withdraw(Context context, Recipient recipient) public {
+    Controller controller = Controller.wrap(msg.sender);
+    _withdraw(controller, context, recipient);
+  }
+
+  function withdrawByRecipient(Controller controller, Context context) public {
+    Recipient recipient = Recipient.wrap(msg.sender);
+    _withdraw(controller, context, recipient);
   }
 }
