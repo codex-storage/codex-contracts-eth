4c7a91303556638ce5154c9c83ebbaba746c45d2
Mark Spanbroek <mark@spanbroek.net>
2025-02-06 10:58:21 +0100

    vault: burn entire fund

--- contracts/vault/Locks.sol
+++ contracts/vault/Locks.sol
@@ -7,10 +7,27 @@ struct Lock {
   Timestamp expiry;
   Timestamp maximum;
   uint128 value;
+  bool burned;
+}
+
+enum LockStatus {
+  NoLock,
+  Locked,
+  Unlocked,
+  Burned
 }
 
 library Locks {
-  function isLocked(Lock memory lock) internal view returns (bool) {
-    return Timestamps.currentTime() < lock.expiry;
+  function status(Lock memory lock) internal view returns (LockStatus) {
+    if (lock.burned) {
+      return LockStatus.Burned;
+    }
+    if (Timestamps.currentTime() < lock.expiry) {
+      return LockStatus.Locked;
+    }
+    if (lock.maximum == Timestamp.wrap(0)) {
+      return LockStatus.NoLock;
+    }
+    return LockStatus.Unlocked;
   }
 }
