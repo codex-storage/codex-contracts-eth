ef87c365ee0e7215e6f2b2aa1b166053d05c107d
Mark Spanbroek <mark@spanbroek.net>
2025-02-20 14:59:41 +0100

    vault: allow for multiple accounts for one account holder

--- contracts/vault/Accounts.sol
+++ contracts/vault/Accounts.sol
@@ -4,6 +4,10 @@ pragma solidity 0.8.28;
 import "./TokenFlows.sol";
 import "./Timestamps.sol";
 
+/// Used to identify an account. The first 20 bytes consist of the address of
+/// the account holder, and the last 12 bytes consist of a discriminator value.
+type AccountId is bytes32;
+
 /// Records the token balance and the incoming and outgoing token flows
 struct Account {
   Balance balance;
@@ -36,6 +40,26 @@ library Accounts {
   using Accounts for Account;
   using TokenFlows for TokensPerSecond;
   using Timestamps for Timestamp;
+
+  /// Creates an account id from the account holder address and a discriminator.
+  /// The discriminiator can be used to create different accounts that belong to
+  /// the same account holder.
+  function encodeId(
+    address holder,
+    bytes12 discriminator
+  ) internal pure returns (AccountId) {
+    bytes32 left = bytes32(bytes20(holder));
+    bytes32 right = bytes32(uint256(uint96(discriminator)));
+    return AccountId.wrap(left | right);
+  }
+
+  /// Extracts the account holder and the discriminator from the the account id
+  function decodeId(AccountId id) internal pure returns (address, bytes12) {
+    bytes32 unwrapped = AccountId.unwrap(id);
+    address holder = address(bytes20(unwrapped));
+    bytes12 discriminator = bytes12(uint96(uint256(unwrapped)));
+    return (holder, discriminator);
+  }
 
   /// Calculates whether the available balance is sufficient to sustain the
   /// outgoing flow of tokens until the specified timestamp
