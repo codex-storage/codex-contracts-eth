343cc178af1a913bf20d12a8151d3edc8c55f148
Adam Uhlíř <adam@uhlir.dev>
2025-06-19 13:04:24 +0200

    feat: upgradable Marketplace contract

--- ignition/modules/periods.js
+++ ignition/modules/periods.js
@@ -4,9 +4,11 @@ module.exports = buildModule("Periods", (m) => {
   const deployer = m.getAccount(0)
   const secondsPerPeriod = m.getParameter("secondsPerPeriod", 0)
 
-  const periods = m.contract("Periods", [secondsPerPeriod], {
+  const periods = m.contract("TestPeriods", [], {
     from: deployer,
   })
+
+  m.call(periods, "initialize", [secondsPerPeriod]);
 
   return { periods }
 })
